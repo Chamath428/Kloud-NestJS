c1303aa6529faa77e906b093498f94bccbca415b
Chamath428 <chamath428@gmail.com>
2024-06-11 21:28:50 +0530

    add-users-and-post-files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { PostsController } from './posts/posts.controller';
+import { PostsService } from './posts/posts.service';
+import { PostsModule } from './posts/posts.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, PostsModule],
+  controllers: [AppController, UsersController, PostsController],
+  providers: [AppService, PostsService],
 })
 export class AppModule {}
