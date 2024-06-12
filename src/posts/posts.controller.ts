915417e575743e6b56eeb9b2ac1df085be5d6478
Chamath428 <chamath428@gmail.com>
2024-06-12 11:27:58 +0530

    add auth routs to post end points

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,32 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post,UseGuards } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post-dto';
 import { UpdatePostDto } from './dto/update-post-dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('posts')
 export class PostsController {
     constructor(private readonly postService: PostsService) {}
 
+    @UseGuards(AuthGuard)
     @Post()
     create (@Body() createPostDto:CreatePostDto){
         return this.postService.create(createPostDto)
     }
 
+    @UseGuards(AuthGuard)
     @Get()
     findAll(){
         return this.postService.findAll()
     }
 
+    @UseGuards(AuthGuard)
     @Get(':id')
     findOne(@Param('id') id:number){
         return this.postService.findOne(id)
     }
 
+    @UseGuards(AuthGuard)
     @Patch()
     update(@Body() updatePostDto:UpdatePostDto){
         return this.update(updatePostDto)
     }
 
+    @UseGuards(AuthGuard)
     @Delete(':id')
     remove(@Param('id') id:number){
         this.postService.remove(id)
